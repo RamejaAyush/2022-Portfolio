154bd6e94e98260bc8be16c749b1329790649473
Ayush Rameja <ayushrwork@gmail.com>
2023-04-23 21:50:10 +0530

    Added Gsap animations to the loader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './Styles/Global.scss';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,7 +1,89 @@
+import { gsap } from 'gsap';
+import '../Styles/Homepage.scss';
+import { useEffect, useRef, useState } from 'react';
+
 const Homepage = () => {
-  return (
+  const loaderTitle = useRef(null);
+  const loaderSubTitle = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeline = gsap.timeline({
+      onComplete: () => {
+        setLoading(false);
+      },
+    });
+
+    timeline
+      .fromTo(
+        loaderTitle.current,
+        {
+          duration: 1,
+          opacity: 0,
+          y: 50,
+          ease: 'power3.out',
+          delay: 0.5,
+        },
+        {
+          duration: 1,
+          opacity: 1,
+          y: 0,
+          ease: 'power3.out',
+          delay: 0.5,
+        }
+      )
+      .fromTo(
+        loaderSubTitle.current,
+        {
+          duration: 1,
+          opacity: 0,
+          y: 50,
+          ease: 'power3.in',
+          delay: 0.5,
+        },
+        {
+          duration: 1,
+          opacity: 1,
+          y: 0,
+          ease: 'power3.in',
+          delay: 0.5,
+        },
+        '-=1.5'
+      )
+      .to(
+        loaderTitle.current,
+        {
+          duration: 1,
+          opacity: 0,
+          y: -50,
+          ease: 'power3.out',
+        },
+        '+=1'
+      )
+      .to(
+        loaderSubTitle.current,
+        {
+          duration: 1,
+          opacity: 0,
+          y: -50,
+          ease: 'power3.out',
+        },
+        '-=0.5'
+      );
+  });
+
+  return loading ? (
+    <div className="loader">
+      <h1 className="logo" ref={loaderTitle}>
+        Ayush Rameja
+      </h1>
+      <h1 className="title" ref={loaderSubTitle}>
+        Web Developer • UI Designer
+      </h1>
+    </div>
+  ) : (
     <div className="homepage">
-      <h1>Homepage</h1>
+      <h2>Homepage</h2>
     </div>
   );
 };
